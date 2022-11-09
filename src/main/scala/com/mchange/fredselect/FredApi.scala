8ee81b1cc874184fea3c2567d5dc88f0f6fe13fa
Steve Waldman <swaldman@mchange.com>
2022-11-09 13:28:53 -0500

    Oops! Get rid of the letter 'A' accidentally appended to series names when hitting the FRED API.

--- src/main/scala/com/mchange/fredselect/FredApi.scala
+++ src/main/scala/com/mchange/fredselect/FredApi.scala
@@ -54,7 +54,8 @@ object FredApi:
   // don't blame me for the seriess thing, it is straight out of the Fred API
   def seriesMetadataFetch( series : String, apiKey : String ) : ZIO[Any, Throwable, FredApi.SeriesMetadata] = 
     HttpClientZioBackend().flatMap { backend =>
-      val request = basicRequest.get(uri"https://api.stlouisfed.org/fred/series?series_id=${series}A&api_key=${apiKey}&file_type=json")
+      val request = basicRequest.get(uri"https://api.stlouisfed.org/fred/series?series_id=${series}&api_key=${apiKey}&file_type=json")
+      //println(request)
       for {
         response        <- request.response(asJson[SeriessMetadata]).send(backend)
         seriessMetadata <- ZIO.fromEither(response.body)
@@ -64,7 +65,8 @@ object FredApi:
 
   def seriesObservationsFetch( series : String, apiKey : String ) : ZIO[Any, Throwable, FredApi.SeriesObservations] = 
     HttpClientZioBackend().flatMap { backend =>
-      val request = basicRequest.get(uri"https://api.stlouisfed.org/fred/series/observations?series_id=${series}A&api_key=${apiKey}&file_type=json")
+      val request = basicRequest.get(uri"https://api.stlouisfed.org/fred/series/observations?series_id=${series}&api_key=${apiKey}&file_type=json")
+      //println(request)
       for {
         response           <- request.response(asJson[SeriesObservations]).send(backend)
         seriesObservations <- ZIO.fromEither(response.body)
